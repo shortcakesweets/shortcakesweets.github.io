192fedb48f334d55ac265d46ce6ec6bfe9dfaedb
Shortcake <olleh2020@kaist.ac.kr>
2024-01-18 01:07:04 +0900

    made page

--- solvedac_tutorial/solvedac_tutorial.js
+++ solvedac_tutorial/solvedac_tutorial.js
@@ -1,25 +1,43 @@
+const baseURL = "https://solved.ac/api/v3";
+const endpoint_getProblemById = "/problem/show";
+
+function getProblemById(problemId) {
+	const url = baseURL + endpoint_getProblemById + "/" + problemId;
+	return fetch(url, {mode : 'no-cors'})
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+			return response.json();
+		})
+		.then((data) => {})
+		.catch((error) => {
+			console.error("Fetch error:", error);
+		});
+}
+
 function addTable(filename) {
-    fetch(filename)
-        .then(response => response.json())
-        .then(data => {
-            const tableParentDiv = document.getElementById("tableParent");
+	fetch(filename)
+		.then((response) => response.json())
+		.then((data) => {
+			const tableParentDiv = document.getElementById("tableParent");
 
-            const table = document.createElement("table");
-            const titleRow = document.createElement("tr");
-            const td_probId = document.createElement("td");
-            td_probId.innerHTML = "문제 번호";
-            const td_probName = document.createElement("td");
-            td_probName.innerHTML = "문제 이름";
-            const td_solved = document.createElement("td");
-            td_solved.innerHTML = "풀이 현황";
-            titleRow.appendChild(td_probId);
-            titleRow.appendChild(td_probName);
-            titleRow.appendChild(td_solved);
-            table.appendChild(titleRow);
+			const table = document.createElement("table");
+			const titleRow = document.createElement("tr");
+			const td_probId = document.createElement("td");
+			td_probId.innerHTML = "문제 번호";
+			const td_probName = document.createElement("td");
+			td_probName.innerHTML = "문제 이름";
+			const td_solved = document.createElement("td");
+			td_solved.innerHTML = "풀이 현황";
+			titleRow.appendChild(td_probId);
+			titleRow.appendChild(td_probName);
+			titleRow.appendChild(td_solved);
+			table.appendChild(titleRow);
 
-            data.numbers.forEach(probId => {
-                const row = document.createElement("tr");
-                /*
+			data.numbers.forEach((probId) => {
+				const row = document.createElement("tr");
+				/*
                 for (let i = 0; i < 3; i++) {
                     const td = document.createElement("td");
                     if (i == 0) td.textContent = probId;
@@ -27,44 +45,42 @@ function addTable(filename) {
                 }
                 table.appendChild(row);
                 */
-                const td1 = document.createElement("td");
-                td1.textContent = probId;
-                row.appendChild(td1);
+				const td1 = document.createElement("td");
+				td1.textContent = probId;
+				row.appendChild(td1);
 
-                const td2 = document.createElement("td");
-                const hyperLink = document.createElement("a");
-                hyperLink.href = "https://www.acmicpc.net/problem/" + probId;
-                fetch("https://solved.ac/api/v3/problem/show?problemId=" + probId)
-                    .then(response => response.json())
-                    .then(data => {
-                        const titleKo = data.titleKo;
-                        hyperLink.innerHTML = titleKo;
-                    });
+				const td2 = document.createElement("td");
+				const hyperLink = document.createElement("a");
+				hyperLink.href = "https://www.acmicpc.net/problem/" + probId;
+				fetch("https://solved.ac/api/v3/problem/show?problemId=" + probId)
+					.then((response) => response.json())
+					.then((data) => {
+						const titleKo = data.titleKo;
+						hyperLink.innerHTML = titleKo;
+					});
+			});
 
+			const wrapperSummary = document.createElement("summary");
+			wrapperSummary.innerHTML = data.title;
 
-            });
+			const wrapperDetails = document.createElement("details");
+			wrapperDetails.appendChild(wrapperSummary);
+			wrapperDetails.appendChild(table);
 
-            const wrapperSummary = document.createElement("summary");
-            wrapperSummary.innerHTML = data.title;
+			const wrapperDiv = document.createElement("div");
+			wrapperDiv.classList.add("mainContent");
+			wrapperDiv.appendChild(wrapperDetails);
 
-            const wrapperDetails = document.createElement("details");
-            wrapperDetails.appendChild(wrapperSummary);
-            wrapperDetails.appendChild(table);
-
-            const wrapperDiv = document.createElement("div");
-            wrapperDiv.classList.add("mainContent");
-            wrapperDiv.appendChild(wrapperDetails);
-
-            tableParentDiv.appendChild(wrapperDiv);
-        });
+			tableParentDiv.appendChild(wrapperDiv);
+		});
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("filename.json")
-        .then(response => response.json())
-        .then(data => {
-            data.filename_list.forEach(filename => {
-                addTable(filename);
-            });
-        });
+	fetch("filename.json")
+		.then((response) => response.json())
+		.then((data) => {
+			data.filename_list.forEach((filename) => {
+				addTable(filename);
+			});
+		});
 });
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./solvedac_tutorial/*.{html,css}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
